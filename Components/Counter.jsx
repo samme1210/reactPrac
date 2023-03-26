ac76b7ac4ad954009b7f490611d466e27ca702d3
samme1210 <samme1210@gmail.com>
2023-03-26 14:02:03 +0100

    counting apples

--- Components/Counter.jsx
+++ Components/Counter.jsx
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
 
 function Counter() {
-const [count, setCount] = useState(0);
+const [cart, setCart] = useState({
+  item: "apple",
+  quantity: 0,
+});
 
-  function increaseCount() {
-    setCount(count + 1)
+  function addApple() {
+    setCart(prevCart => ({
+      ...prevCart,
+      quantity: prevCart.quantity + 1,
+    }))
   }
 
-  function decreaseCount() {
-    setCount(count - 1)
+  function decreaseApple() {
+    setCart(prevCart => ({
+      ...prevCart,
+      quantity: prevCart.quantity - 1,
+    }))
   }
+  
 
   return (
     <>
-      <button onClick={decreaseCount} className="btn__count">-</button>
-      {count}
-      <button onClick={increaseCount} className="btn__count">+</button>
+      <button onClick={decreaseApple} className="btn__count">-</button>
+      {cart.quantity}
+      {cart.item}
+      <button onClick={addApple} className="btn__count">+</button>
     </>
   );
 }
